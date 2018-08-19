40cbda9d014741515dd0393509ad7b74f6f66032
jitsolution19 <jitsolution19@gmail.com>
2018-08-19 13:31:35 +0530

    updates with bigbasket.com and housing.com

---
+++ FunWithAutomation/src/genericUtility/Highlight_Weblements.java
@@ -0,0 +1,19 @@
+package genericUtility;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.WrapsDriver;
+
+public class Highlight_Weblements {
+
+	public static void highlightElement(WebElement element) throws Exception {
+		for (int i = 0; i < 5; i++) {
+			WrapsDriver wrappedElement = (WrapsDriver) element;
+			JavascriptExecutor driver = (JavascriptExecutor) wrappedElement.getWrappedDriver();
+			driver.executeScript("arguments[0].setAttribute('style',arguments[1]);", element,
+					"color: green; border: 5px solid red;");
+			Thread.sleep(500);
+			driver.executeScript("arguments[0].setAttribute('style',arguments[1]);", element, "");
+		}
+	}
+}
