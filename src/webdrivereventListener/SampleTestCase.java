c98716e57a61844465c94c6fbb32770703233731
Jitendra <jitsolution19@gmail.com>
2018-12-30 22:42:46 +0530

    updated

---
+++ src/webdrivereventListener/SampleTestCase.java
@@ -0,0 +1,190 @@
+package webdrivereventListener;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.events.WebDriverEventListener;
+
+public class SampleTestCase implements WebDriverEventListener{
+
+	@Test
+	public void applicationNavigation()
+	{
+		System.setProperty("webdriver.gecko.driver","E:\\Selenium_Project\\browserdriver\\geckodriver-v0.22.0-win64\\geckodriver.exe");
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+		capabilities.setCapability("Marionette", false);
+		WebDriver driver = new FirefoxDriver(capabilities);
+		driver.navigate().to("https://www.netmeds.com/");
+		driver.manage().timeouts().pageLoadTimeout(20,TimeUnit.SECONDS);
+		System.out.println("Done");
+		driver.close();
+	}
+
+	@Override
+	public void afterAlertAccept(WebDriver driver) {
+	System.out.println("afterAlertAccept is performed sucessfully");
+		
+	}
+
+	@Override
+	public void afterAlertDismiss(WebDriver arg0) {
+	System.out.println("afterAlertDismiss is performed sucessfully");
+		
+	}
+
+	@Override
+	public void afterChangeValueOf(WebElement element, WebDriver driver, CharSequence[] value) {
+//	System.out.println("afterAlertDismiss is performed sucessfully");
+		
+	}
+
+	@Override
+	public void afterClickOn(WebElement arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterFindBy(By arg0, WebElement arg1, WebDriver arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <X> void afterGetScreenshotAs(OutputType<X> arg0, X arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterGetText(WebElement arg0, WebDriver arg1, String arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterNavigateBack(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterNavigateForward(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterNavigateRefresh(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterNavigateTo(String arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterScript(String arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void afterSwitchToWindow(String arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeAlertAccept(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeAlertDismiss(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeChangeValueOf(WebElement arg0, WebDriver arg1, CharSequence[] arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeClickOn(WebElement arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeFindBy(By arg0, WebElement arg1, WebDriver arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <X> void beforeGetScreenshotAs(OutputType<X> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeGetText(WebElement arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeNavigateBack(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeNavigateForward(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeNavigateRefresh(WebDriver arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeNavigateTo(String arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeScript(String arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void beforeSwitchToWindow(String arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onException(Throwable arg0, WebDriver arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+}
